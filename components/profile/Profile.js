ea7bab64665ea6ae5271a2827bd72d2d953350aa
Muhammatsorin <sorin.13122540@gmail.com>
2023-02-13 17:56:59 +0700

    deploy / test 3

--- components/profile/Profile.js
+++ components/profile/Profile.js
@@ -22,7 +22,79 @@ const Profile = (props) => {
 
     return (
         <>
-            
+            <Profile_Wrapper>
+                <Container maxWidth="lg" className={styles.container}>
+                    <Box sx={{ width: '100%' }} />
+                    <h1 className={styles.title} id={props.id}>profile</h1>
+                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} className={styles.row1}>
+                        <Grid item xs={12} md={6} className={styles.grid1}>
+                            <Item className={styles.item1}>
+                                <h1>My Profile</h1>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} md={6} className={styles.grid2}>
+                            <Item className={styles.item2}>
+                                <div className={styles.details}>
+                                    <p className={styles.pt}>Name :</p>
+                                    <p className={styles.pd}>Muhammatsorin Hawae</p>
+                                </div>
+                                <div className={styles.details}>
+                                    <p className={styles.pt}>Date of Birth :</p>
+                                    <p className={styles.pd}>December 13, 1997</p>
+                                </div>
+                                <div className={styles.details}>
+                                    <p className={styles.pt}>Email :</p>
+                                    <p className={styles.pd}>sorin.13122540@gmail.com</p>
+                                </div>
+                                <div className={styles.details}>
+                                    <p className={styles.pt}>Phone :</p>
+                                    <p className={styles.pd}>+669 8029 3887</p>
+                                </div>
+                            </Item>
+                            <Item className={styles.item3}>
+                                <p>I have gone through many stages in my career and each has informed my current work ethic and philosophy.
+                                    I excel in many page design roles in a variety of needs and believe my number one asset is the ability to adapt to any
+                                    challenges thrown away.</p>
+                                <Stack direction="row" spacing={0}>
+                                    <a href="Modern Professional Resume.pdf" download="Modern Professional Resume.pdf">
+                                        <Button variant="contained" className={styles.btn}>Download CV</Button>
+                                    </a>
+                                </Stack>
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} className={styles.row1}>
+                        <Grid item xs={12} md={6} className={styles.grid1}>
+                            <Item className={styles.item1}>
+                                <h1>Education</h1>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} md={6} className={styles.grid2}>
+                            <Item className={styles.my_educate}>
+                                <div className={styles.education}>
+                                    <div className={styles.university}>
+                                        <h3 >2018 - 2022</h3>
+                                        <p>Prince of Songkla University, Phuket Campus</p>
+                                    </div>
+                                    <div className={styles.major}>
+                                        <p>Faculty of Engineering, Major Computer Engineer, Phuket, Thailand</p>
+                                    </div>
+                                </div>
+                                <div className={styles.education}>
+                                    <div className={styles.university}>
+                                        <h3 >2013 - 2018</h3>
+                                        <p>Darunsat Witya School</p>
+                                    </div>
+                                    <div className={styles.major}>
+                                        <p>Science - Mathematics Programme, Pattani, Thailand</p>
+                                    </div>
+                                </div>
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Box />
+                </Container>
+            </Profile_Wrapper>
         </>
     )
 }
