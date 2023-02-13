1b2a23f4538b05e7bf46e6902a07435dd8c3c790
Muhammatsorin <sorin.13122540@gmail.com>
2023-02-13 20:04:59 +0700

    deploy / test 11

--- components/title/Title.js
+++ components/title/Title.js
@@ -4,11 +4,18 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Styled from 'styled-components'
+import { styled } from '@mui/material/styles';
 import Img from 'next/image'
 import Divider from '@mui/material/Divider';
 import { GrInstagram, GrFacebook } from "react-icons/gr";
+import Paper from '@mui/material/Paper';
 
 const Title = (props) => {
+
+    const Item = styled(Paper)(({ theme }) => ({
+        boxShadow: 'none',
+        fontFamily: 'Montserrat-bold',
+    }));
 
     const openFacebook = () => {
         window.open('https://web.facebook.com/sorin.hawae/')
@@ -27,35 +34,39 @@ const Title = (props) => {
                         <div className='grid-wrap'>
                             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} style={{ display: 'flex', alignItems: 'center' }} className="row-1">
                                 <Grid xs={12} md={6} className="row-1-col-1">
-                                    <div className='img'>
-                                        <Img src='/images/me.jpg'
-                                            width={500}
-                                            height={400}
-                                            priority
-                                            alt='me.jpg'
-                                        />
-                                    </div>
+                                    <Item>
+                                        <div className='img'>
+                                            <Img src='/images/me.jpg'
+                                                width={500}
+                                                height={400}
+                                                priority
+                                                alt='me.jpg'
+                                            />
+                                        </div>
+                                    </Item>
                                 </Grid>
                                 <Grid xs={12} md={6} className="row-1-col-2">
-                                    <div className='title-details'>
-                                        <div className='my-name'>
-                                            <h2>Hi, I'm </h2>
-                                            <h1>SOREEN</h1>
+                                    <Item>
+                                        <div className='title-details'>
+                                            <div className='my-name'>
+                                                <h2>Hi, I'm </h2>
+                                                <h1>SOREEN</h1>
+                                            </div>
+                                            <div className='job'>
+                                                <h3>FRONTEND DEVELOPER</h3>
+                                            </div>
+                                            <p>Hello, My name is Muhammatsorin Hawae. I'm Frontend developer. Development and learning new things It's what made me grow even more.</p>
                                         </div>
-                                        <div className='job'>
-                                            <h3>FRONTEND DEVELOPER</h3>
+                                        <Divider />
+                                        <div className='social-icons'>
+                                            <div className='facebook' onClick={openFacebook}>
+                                                <GrFacebook />
+                                            </div>
+                                            <div className='instragram' onClick={openIG}>
+                                                <GrInstagram />
+                                            </div>
                                         </div>
-                                        <p>Hello, My name is Muhammatsorin Hawae. I'm Frontend developer. Development and learning new things It's what made me grow even more.</p>
-                                    </div>
-                                    <Divider />
-                                    <div className='social-icons'>
-                                        <div className='facebook' onClick={openFacebook}>
-                                            <GrFacebook />
-                                        </div>
-                                        <div className='instragram' onClick={openIG}>
-                                            <GrInstagram />
-                                        </div>
-                                    </div>
+                                    </Item>
                                 </Grid>
                             </Grid>
                         </div>
